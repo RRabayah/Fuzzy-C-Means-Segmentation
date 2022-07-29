b7b76e6015862e2568024e91ea09c0a332cc9711
RRabayah <Russl2004@gmail.com>
2022-07-29 21:08:38 +0400

    all files in directory

--- ImgProcessing.py
+++ ImgProcessing.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import os
+
+original_directory = "Images/Original"
+modified_directory = "Images/Modified"
 
 
 class ImgProcessor:
@@ -9,3 +13,10 @@ class ImgProcessor:
         self.img = Image.open(img)
         img_resized = self.img.resize(self.size)
         im_resize = img_resized.save("resized_img.jpg")
+
+
+    def resize_all(self):
+        for filename in os.listdir(original_directory):
+            f = os.path.join(original_directory, filename)
+            if os.path.isfile(f):
+                self.resize(f)
