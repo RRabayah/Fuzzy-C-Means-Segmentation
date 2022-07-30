173f47302cd644e63990653057dd76c607d15cd3
RRabayah <Russl2004@gmail.com>
2022-07-30 18:03:28 +0400

    Inverted

--- ImgProcessing.py
+++ ImgProcessing.py
@@ -16,11 +16,12 @@ class ImgProcessor:
         im_gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         th, im_gray_th_otsu = cv2.threshold(im_gray, 128, 192, cv2.THRESH_OTSU)
         #cv2.resize(im_gray_th_otsu, self.size)
+        inverted_image = self.invert(im_gray_th_otsu)
         os.chdir(modified_directory)
-        cv2.imwrite(filename, im_gray_th_otsu)
+        cv2.imwrite(filename, inverted_image)
 
-
-
+    def invert(self, image):
+        return 255 - image
 
     def modify_all(self):
         for filename in os.listdir(original_directory):
